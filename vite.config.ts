4ec5243cc7b52a88c99a10dda19f72ec161ddd00
usman.shahid <usman.shahid@grp.contegris.com>
2025-12-27 21:27:16 +0500

    build

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base: "/sparky-react-build/",
+  base:"/sparky-react-build/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
